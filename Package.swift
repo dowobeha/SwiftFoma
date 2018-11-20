f522259e8283b392d9e8fb87717881c2cc8c42ba
Lane Schwartz <dowobeha@gmail.com>
2018-11-20 13:57:57 -0800

    Removing stuff from foma.c in CFoma

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/dowobeha/CFoma.git" /* package url */, from: "0.0.51"),
+        .package(url: "https://github.com/dowobeha/CFoma.git" /* package url */, from: "0.0.56"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
